a7df14380d1aed8b56a721f0dae7ca4694a89386
kdmin0706 <gxxk76@gmail.com>
2023-12-14 14:50:07 +0900

    [feat]: 프로젝트 세팅

---
+++ src/main/java/com/community/amkorea/AmKoreaApplication.java
@@ -0,0 +1,13 @@
+package com.community.amkorea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmKoreaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AmKoreaApplication.class, args);
+  }
+
+}
