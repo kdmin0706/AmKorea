e38a849653b570859b4221c84c91576c79bed857
kdmin0706 <gxxk76@gmail.com>
2023-12-18 00:01:19 +0900

    [feat]: update ErrorCode

--- src/main/java/com/community/amkorea/global/exception/ErrorCode.java
+++ src/main/java/com/community/amkorea/global/exception/ErrorCode.java
@@ -12,7 +12,7 @@ public enum ErrorCode {
    * 400 Bad Request
    */
   PASSWORD_NOT_MATCH(HttpStatus.BAD_REQUEST, "비밀번호가 올바르지 않습니다."),
-
+  INVALID_AUTH_CODE(HttpStatus.BAD_REQUEST, "인증번호가 올바르지 않습니다."),
 
   /**
    * 401 Unauthorized
@@ -23,11 +23,13 @@ public enum ErrorCode {
    * 404 Not Found
    */
   USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
+  EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 이메일입니다"),
 
   /**
    * 409 conflict
    */
   DUPLICATE_USER(HttpStatus.CONFLICT, "이미 존재하는 회원입니다."),
+
 
   /**
    * 500 Internal Server Error
