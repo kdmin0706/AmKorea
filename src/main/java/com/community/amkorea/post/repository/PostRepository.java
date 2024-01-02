e350c150e8ad3a5140e88ed3666f9ed3727282a1
kdmin0706 <gxxk76@gmail.com>
2024-01-02 19:53:52 +0900

    [refactor]: 페이징 JPA -> JPQL

--- src/main/java/com/community/amkorea/post/repository/PostRepository.java
+++ src/main/java/com/community/amkorea/post/repository/PostRepository.java
@@ -3,11 +3,15 @@ package com.community.amkorea.post.repository;
 import com.community.amkorea.post.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> , CustomPostRepository{
+
+  @Query("SELECT p FROM Post p WHERE p.content LIKE %:content%"
+      + " ORDER BY p.id DESC, p.views DESC, p.likeCount DESC")
   Page<Post> findAllByContentContaining(String content, Pageable pageable);
 }
 
