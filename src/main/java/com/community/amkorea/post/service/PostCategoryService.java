83baf3965d62af2a6260fc3d685863d7611e0e42
kdmin0706 <gxxk76@gmail.com>
2024-01-03 23:27:11 +0900

    [refactor]: #25 피드백 반영

--- src/main/java/com/community/amkorea/post/service/PostCategoryService.java
+++ src/main/java/com/community/amkorea/post/service/PostCategoryService.java
@@ -3,7 +3,7 @@ package com.community.amkorea.post.service;
 import com.community.amkorea.post.dto.PostCategoryRequest;
 import com.community.amkorea.post.dto.PostCategoryResponse;
 import com.community.amkorea.post.dto.PostResponse;
-import java.util.List;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface PostCategoryService {
@@ -11,5 +11,5 @@ public interface PostCategoryService {
 
   PostCategoryResponse updateCategory(Long id, String username, PostCategoryRequest request);
 
-  List<PostResponse> getCategory(Long id, Pageable pageable);
+  Page<PostResponse> getCategory(Long id, Pageable pageable);
 }
