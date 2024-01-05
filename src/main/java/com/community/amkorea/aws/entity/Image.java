82c4034148c5e18ef0ef0a3aed9f3d69700205ae
kdmin0706 <gxxk76@gmail.com>
2024-01-05 15:09:46 +0900

    [feat]: #27 AWS S3 파일 업로드 추가

---
+++ src/main/java/com/community/amkorea/aws/entity/Image.java
@@ -0,0 +1,46 @@
+package com.community.amkorea.aws.entity;
+
+import com.community.amkorea.global.entity.BaseEntity;
+import com.community.amkorea.post.entity.Post;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Image extends BaseEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String url;
+
+  @Column(nullable = false)
+  private String fileName;
+
+  @Column(nullable = false)
+  private Long size;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "post_id")
+  private Post post;
+
+  public void mappingPost(Post post) {
+    this.post = post;
+  }
+
+}
