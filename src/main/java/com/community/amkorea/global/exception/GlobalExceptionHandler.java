86db056af2b4b5465440644d7230ae26a37d2cc5
kdmin0706 <gxxk76@gmail.com>
2024-01-09 10:08:24 +0900

    [refactor]: EnumType 리팩토링 진행

--- src/main/java/com/community/amkorea/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/community/amkorea/global/exception/GlobalExceptionHandler.java
@@ -2,10 +2,12 @@ package com.community.amkorea.global.exception;
 
 import static com.community.amkorea.global.exception.ErrorCode.INTERNAL_SERVER_ERROR;
 import static com.community.amkorea.global.exception.ErrorCode.INVALID_REQUEST;
+import static com.community.amkorea.global.exception.ErrorCode.REQUEST_PARAM_NOT_MATCH;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -25,11 +27,17 @@ public class GlobalExceptionHandler {
    */
   @ExceptionHandler(MethodArgumentNotValidException.class)
   public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-    log.error("CustomException is occurred." + e);
+    log.error("handleMethodArgumentNotValidException is occurred." + e);
     return new ErrorResponse(INVALID_REQUEST,
         INVALID_REQUEST.getHttpStatus(), INVALID_REQUEST.getDescription());
   }
 
+  @ExceptionHandler(MissingServletRequestParameterException.class)
+  public ErrorResponse handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
+    log.error("handleMissingServletRequestParameterException is occurred." + e);
+    return new ErrorResponse(REQUEST_PARAM_NOT_MATCH,
+        REQUEST_PARAM_NOT_MATCH.getHttpStatus(), REQUEST_PARAM_NOT_MATCH.getDescription());
+  }
 
   @ExceptionHandler(Exception.class)
   public ErrorResponse handleException(Exception e) {
